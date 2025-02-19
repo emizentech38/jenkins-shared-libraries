a7cc0aeb65db39e4cb78d1ba708176892653fe30
emizentech38 <emizentech38@gmail.com>
2025-02-19 17:47:31 +0530

    Create push.groovy

---
+++ vars/push.groovy
@@ -0,0 +1,13 @@
+def pushDockerImage(String projectName, String imageTag, String credentialsId) {
+    echo "This is pushing the image to Docker Hub"
+
+    withCredentials([usernamePassword(credentialsId: credentialsId, 
+                                      usernameVariable: 'DOCKER_HUB_USER', 
+                                      passwordVariable: 'DOCKER_HUB_PASS')]) {
+        sh """
+            docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASS}
+            docker image tag ${projectName}:${imageTag} ${DOCKER_HUB_USER}/${projectName}:${imageTag}
+            docker push ${DOCKER_HUB_USER}/${projectName}:${imageTag}
+        """
+    }
+}
